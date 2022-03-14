49d42ba5261c1177129a5afd42db227e50a2c990
radenadhyaksa <raksa69@gmail.com>
2022-03-14 11:03:29 +0700

    update - to TipeData var

--- lib/variable.dart
+++ lib/variable.dart
@@ -1,5 +1,6 @@
 void main() {
-  String name = 'Raden Adhyaksa Rahardjo';
+  //String name = 'Raden Adhyaksa Rahardjo'; //deklarasi masih menentukan TipeData secara manual
+  var name = 'Raden Adhyaksa Rahardjo'; // Tipe data sudah otomatis menyesuaikan nilai variable nya, sehingga tidak perlu menyebutkan TipeDatanya lagi.
 
   print(name);
 }
