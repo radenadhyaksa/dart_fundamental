9a4dafd44bcac281a18642d306be2a65d0511efe
radenadhyaksa <raksa69@gmail.com>
2022-03-14 11:13:38 +0700

    update - to deklarasi with final

--- lib/variable.dart
+++ lib/variable.dart
@@ -3,4 +3,18 @@ void main() {
   var name = 'Raden Adhyaksa Rahardjo'; // Tipe data sudah otomatis menyesuaikan nilai variable nya, sehingga tidak perlu menyebutkan TipeDatanya lagi.
 
   print(name);
+
+  ///pembatas
+  print('=====================');
+  
+  ///deklarasi
+  var firtsName = 'Raden';
+  final lastName = 'Adhyaksa';
+
+  ///merubah nilai variable
+  firtsName = 'firtname Raden';
+  //lastName = 'lastName Adhyaksa'; //hasil akan error (karena final tidak bisa dirubah/dideklarasikan ulang)
+  
+  print(firtsName);
+  print(lastName);
 }
